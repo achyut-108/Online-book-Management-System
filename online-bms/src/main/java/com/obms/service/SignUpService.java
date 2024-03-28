508ed42f8748c4bc5af9a4790a32255c74ea324e
Siddhartha Kumar <siddharthakumar@Siddharthas-MacBook-Air.local>
2024-03-28 15:52:41 +0530

    removing the unwanted files

--- online-bms/src/main/java/com/obms/service/SignUpService.java
+++
@@ -1,9 +0,0 @@
-package com.obms.service;
-
-import com.obms.domain.inventory.NewJoineeSignUpRequest;
-import com.obms.domain.inventory.NewJoineeSignUpResponse;
-
-public interface SignUpService {
-
-	public NewJoineeSignUpResponse signUp(NewJoineeSignUpRequest newJoineeSignUpRequest);
-}
